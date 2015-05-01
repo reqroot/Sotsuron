3097a7099eddb748192f98c0ab3902a3b4b53a0d
1211091 <1211091@A40129.student.ncs.ac.jp>
2015-05-01 16:06:44 +0900

    JinjiTestクラス　追加

---
+++ src/jinji/JinjiTest.java
@@ -0,0 +1,12 @@
+package jinji;
+
+public class JinjiTest {
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("JinjiTest []");
+		return builder.toString();
+	}
+
+}
