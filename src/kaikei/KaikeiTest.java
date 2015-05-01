c17109741bf64f3129ed6efb1fe0beed1f30dfce
reqroot <kei.springwind@gmail.com>
2015-05-01 16:03:48 +0900

    KaikeiTestクラスの追加

---
+++ src/kaikei/KaikeiTest.java
@@ -0,0 +1,12 @@
+package kaikei;
+
+public class KaikeiTest {
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("KaikeiTest []");
+		return builder.toString();
+	}
+
+}
