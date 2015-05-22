8c0df7217e540f9d9ec6ba2386559ec03e69ae44
mabuchan <mabuchan@192.168.1.3>
2015-05-23 03:49:49 +0900

    staffInfo.java 変数名調整

--- src/jinji/db/staffInfo.java
+++ src/jinji/db/staffInfo.java
@@ -11,84 +11,76 @@ import java.io.Serializable;
  *
  */
 	public class staffInfo implements Serializable {
-		private String staffID;
-		private String staffName;
-		private String departmentName;
-		private String birthDay;
-		private String baseSalary;
-
-	/**
-	 *
-	 * @param staffID
-	 * @param staffName
-	 * @param departmentName
-	 * @param birthDay
-	 * @param baseSalary
-	 */
-	public staffInfo(String staffID, String staffName, String departmentName,
-			String birthDay, String baseSalary) {
-		super();
-		this.staffID = staffID;
-		this.staffName = staffName;
-		this.departmentName = departmentName;
-		this.birthDay = birthDay;
-		this.baseSalary = baseSalary;
-	}
-
-	public String getStaffID() {
-		return staffID;
-	}
-
-	public void setStaffID(String staffID) {
-		this.staffID = staffID;
-	}
-
-	public String getStaffName() {
-		return staffName;
-	}
-
-	public void setStaffName(String staffName) {
-		this.staffName = staffName;
-	}
-
-	public String getDepartmentName() {
-		return departmentName;
-	}
-
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
-
-	public String getBirthDay() {
-		return birthDay;
-	}
-
-	public void setBirthDay(String birthDay) {
-		this.birthDay = birthDay;
-	}
-
-	public String getBaseSalary() {
-		return baseSalary;
-	}
-
-	public void setBaseSalary(String baseSalary) {
-		this.baseSalary = baseSalary;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("staffInfo [staffID=");
-		builder.append(staffID);
-		builder.append(", staffName=");
-		builder.append(staffName);
-		builder.append(", departmentName=");
-		builder.append(departmentName);
-		builder.append(", birthDay=");
-		builder.append(birthDay);
-		builder.append(", baseSalary=");
-		builder.append(baseSalary);
-		builder.append("]");
-		return builder.toString();
-	}
+		private String staff_id;
+		private String staff_name;
+		private String department_name;
+		private String position_name;
+		private String birthday;
+		private String base_salary;
+		public staffInfo(){}
+		public staffInfo(String staff_id, String staff_name,
+				String department_name, String position_name, String birthday,
+				String base_salary) {
+			super();
+			this.staff_id = staff_id;
+			this.staff_name = staff_name;
+			this.department_name = department_name;
+			this.position_name = position_name;
+			this.birthday = birthday;
+			this.base_salary = base_salary;
+		}
+		public String getStaff_id() {
+			return staff_id;
+		}
+		public void setStaff_id(String staff_id) {
+			this.staff_id = staff_id;
+		}
+		public String getStaff_name() {
+			return staff_name;
+		}
+		public void setStaff_name(String staff_name) {
+			this.staff_name = staff_name;
+		}
+		public String getDepartment_name() {
+			return department_name;
+		}
+		public void setDepartment_name(String department_name) {
+			this.department_name = department_name;
+		}
+		public String getPosition_name() {
+			return position_name;
+		}
+		public void setPosition_name(String position_name) {
+			this.position_name = position_name;
+		}
+		public String getBirthday() {
+			return birthday;
+		}
+		public void setBirthday(String birthday) {
+			this.birthday = birthday;
+		}
+		public String getBase_salary() {
+			return base_salary;
+		}
+		public void setBase_salary(String base_salary) {
+			this.base_salary = base_salary;
+		}
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("staffInfo [staff_id=");
+			builder.append(staff_id);
+			builder.append(", staff_name=");
+			builder.append(staff_name);
+			builder.append(", department_name=");
+			builder.append(department_name);
+			builder.append(", position_name=");
+			builder.append(position_name);
+			builder.append(", birthday=");
+			builder.append(birthday);
+			builder.append(", base_salary=");
+			builder.append(base_salary);
+			builder.append("]");
+			return builder.toString();
+		}
 }
