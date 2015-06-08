3ac6d803193d10898beea11fa38770637fa6aabd
1211091 <1211091@A40129.student.ncs.ac.jp>
2015-06-08 12:34:24 +0900

    取得済み資格用メソッド追加

--- src/jinji/db/license/licenseInfo.java
+++ src/jinji/db/license/licenseInfo.java
@@ -3,6 +3,7 @@ package jinji.db.license;
 import java.io.Serializable;
 
 public class licenseInfo implements Serializable {
+
 	private String license_id;
 	private String license_name;
 
@@ -27,4 +28,5 @@ public class licenseInfo implements Serializable {
 	public void setLicense_name(String license_name) {
 		this.license_name = license_name;
 	}
+
 }
--- src/jinji/db/license/licenseManage.java
+++ src/jinji/db/license/licenseManage.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import jinji.db.staff.staffInfo;
 import db.DBAccess;
 
 /**
@@ -17,17 +18,35 @@ public class licenseManage extends DBAccess {
 
 	private final static String DRIVER_NAME = "java:comp/env/jdbc/MySqlCon";
 	private String selectSql;
+	private String insertSql;
+	private String slicenseSql;
+	licenseInfo liInfo = null;
 
 	public licenseManage() {
 		super(DRIVER_NAME);
-		selectSql ="SELECT *"
-				+ "FROM tbl_license";
+		StringBuilder sb =new StringBuilder();
+
+				sb.append("SELECT license_id, license_name ");
+				sb.append("FROM tbl_license");
+				selectSql = sb.toString();
+				sb.setLength(0);	//StringBuilderの初期化
+
+				sb.append("INSERT INTO tbl_stafflicense(staff_id, license_id) ");
+				 sb.append("VALUES(?, ?)");
+				 insertSql = sb.toString();
+				 sb.setLength(0);
+
+				 sb.append("SELECT license.license_name ");
+				 sb.append("FROM tbl_staff staff ");
+				 sb.append("LEFT OUTER JOIN(tbl_stafflicense slicense INNER JOIN tbl_license license on slicense.license_id = license.license_id) on staff.staff_id = slicense.staff_id ");
+				 sb.append("WHERE staff.staff_id = ?");
+				 slicenseSql = sb.toString();
+				 sb.setLength(0);
 	}
 
 	public List<licenseInfo> selectLicense() throws Exception{
 
 		List<licenseInfo> list = new ArrayList<licenseInfo>();
-		licenseInfo liInfo = null;
 		connect();
 		createStatement();
 		selectExe(selectSql);
@@ -41,5 +60,42 @@ public class licenseManage extends DBAccess {
 		}
 		disConnect();
 		return list ;
+	}
+
+	public List<licenseInfo> stafflicenseSelect(staffInfo sI) throws Exception{
+		List<licenseInfo> list = new ArrayList<licenseInfo>();
+		connect();
+		createStatement(slicenseSql);
+		getPstmt().setString(1, sI.getStaff_id());
+		selectExe();
+		ResultSet rs = getRsResult();
+		while(rs.next()){
+			liInfo = new licenseInfo(
+					"",
+					rs.getString("license_name")
+					);
+			list.add(liInfo);
+		}
+		disConnect();
+		return list;
+
+	}
+
+	public void licenseUpdate(staffInfo sI, licenseInfo lI,int state) throws Exception{ //0:INSERT 1:UPDATE 2:DELETE
+		connect();
+
+		switch(state){
+		case 0 : //INSERT
+			createStatement(insertSql);
+			getPstmt().setString(1, sI.getStaff_id());
+			getPstmt().setString(2, lI.getLicense_id());
+			break;
+		case 1: //UPDATE
+
+		case 2: //DELETE
+		}//switch
+
+		updateExe();
+		disConnect();
 	}
 }
