b7e3cd04cfbe78621a62cd50075c0f8e9ea23db2
1211091 <1211091@A40129.student.ncs.ac.jp>
2015-06-12 16:36:08 +0900

    追加・削除操作によるSQL操作の追加

--- src/jinji/db/license/licenseManage.java
+++ src/jinji/db/license/licenseManage.java
@@ -18,10 +18,11 @@ public class licenseManage extends DBAccess {
 
 	private final static String DRIVER_NAME = "java:comp/env/jdbc/MySqlCon";
 	private String selectSql;
-	private String insertSql;
 	private String slicenseSql;
+	private String conflicense;
+	private String insertSql;
+	private String deleteSql;
 	licenseInfo liInfo = null;
-
 	public licenseManage() {
 		super(DRIVER_NAME);
 		StringBuilder sb =new StringBuilder();
@@ -31,17 +32,31 @@ public class licenseManage extends DBAccess {
 				selectSql = sb.toString();
 				sb.setLength(0);	//StringBuilderの初期化
 
-				sb.append("INSERT INTO tbl_stafflicense(staff_id, license_id) ");
-				 sb.append("VALUES(?, ?)");
-				 insertSql = sb.toString();
-				 sb.setLength(0);//StringBuilderの初期化
-
-				 sb.append("SELECT license.license_name ");
+				sb.append("SELECT license.license_name ");
 				 sb.append("FROM tbl_staff staff ");
 				 sb.append("LEFT OUTER JOIN(tbl_stafflicense slicense INNER JOIN tbl_license license on slicense.license_id = license.license_id) on staff.staff_id = slicense.staff_id ");
 				 sb.append("WHERE staff.staff_id = ?");
 				 slicenseSql = sb.toString();
 				 sb.setLength(0);//StringBuilderの初期化
+
+
+				 sb.append("SELECT license_name ");
+				 sb.append("FROM tbl_license ");
+				 sb.append("WHERE license_id = ?");
+				 conflicense = sb.toString();
+				 sb.setLength(0);//StringBuilderの初期化
+
+				sb.append("INSERT INTO tbl_stafflicense(staff_id, license_id) ");
+				 sb.append("VALUES(?, ?)");
+				 insertSql = sb.toString();
+				 sb.setLength(0);//StringBuilderの初期化
+
+				 sb.append("DELETE FROM tbl_stafflicense ");
+				 sb.append("WHERE license_id = ?");
+				 deleteSql = sb.toString();
+				 sb.setLength(0);//StringBuilderの初期化
+
+
 	}
 
 	public List<licenseInfo> selectLicense() throws Exception{
@@ -81,6 +96,21 @@ public class licenseManage extends DBAccess {
 
 	}
 
+	public String conflicenseSelect(licenseInfo lI) throws Exception {
+		connect();
+		createStatement(conflicense);
+		getPstmt().setString(1, lI.getLicense_id());
+		selectExe();
+		ResultSet rs = getRsResult();
+		String license_name ="";
+		while(rs.next()){
+			license_name = rs.getString("license_name");
+		}
+		disConnect();
+
+		return license_name;
+	}
+
 	public void licenseUpdate(staffInfo sI, licenseInfo lI,int state) throws Exception{ //0:INSERT 1:UPDATE 2:DELETE
 		connect();
 
@@ -90,9 +120,10 @@ public class licenseManage extends DBAccess {
 			getPstmt().setString(1, sI.getStaff_id());
 			getPstmt().setString(2, lI.getLicense_id());
 			break;
-		case 1: //UPDATE
-
-		case 2: //DELETE
+		case 1: //DELETE
+			createStatement(deleteSql);
+			getPstmt().setString(1,lI.getLicense_id());
+			break;
 		}//switch
 
 		updateExe();
