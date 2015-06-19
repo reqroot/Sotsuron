25683dab62e21397348f1da8054fd1b069a79ee1
1211091 <1211091@A40129.student.ncs.ac.jp>
2015-06-19 16:19:01 +0900

    出退勤確認/確定画面へのページ遷移処理追加

--- src/jinji/Attendance.java
+++ src/jinji/Attendance.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jinji.db.attendance.attendanceInfo;
+import jinji.db.attendance.attendanceManage;
+
 /**
  * Servlet implementation class Attendance
  */
@@ -31,7 +34,10 @@ public class Attendance extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String page_title="";
 		String content_page = "";
+		String msg =""; //サクセスorエラーメッセージ
 		String page = request.getParameter("page");
+		attendanceInfo attendInfo = null;
+		attendanceManage aM = new attendanceManage();
 		ArrayList<String> js = new ArrayList<String>(); //JavaScript用List
 
 		//時刻表示
@@ -40,16 +46,56 @@ public class Attendance extends HttpServlet {
 		 js.add("../js/jquery-1.11.3.js"); //JQuery読み込み
 		 js.add("../js/clock.js"); //時計のJavaScript読み込み
 
-		 //出勤処理
+		 //出勤確認処理
 		 if(request.getParameter("attend") != null){
 			 page_title ="出勤確認画面";
-			 content_page ="/jinji/work_attendance_conf.jsp";
+			 content_page ="/jinji/work_attendance_confirm.jsp";
+			 try {
+				 attendInfo = aM.confattend();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		 }
+
+		 //出勤確定処理
+		 if(request.getParameter("add_in") != null){
+			 try {
+				aM.attendUpdate(0);
+				msg ="出勤完了";
+			} catch (Exception e) {
+				msg ="すでに出勤済みです";
+				e.printStackTrace();
+			}
+		 }
+
+		//退勤確認処理
+		 if(request.getParameter("clockout") != null){
+			 page_title ="退勤確認画面";
+			 content_page ="/jinji/work_attendance_confirm.jsp";
+			 try {
+				 attendInfo = aM.confattend();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		 }
+
+		//退勤確定処理
+		 if(request.getParameter("add_out") != null){
+			 try {
+				aM.attendUpdate(1);
+				msg ="退勤完了";
+			} catch (Exception e) {
+				msg ="退勤失敗です";
+				e.printStackTrace();
+			}
 		 }
 
 		 //JSPにデータを送る準備
 		request.setAttribute("page_title", page_title);
 		request.setAttribute("content_page", content_page);
 		request.setAttribute("js", js);
+		request.setAttribute("attendInfo", attendInfo);
+		request.setAttribute("msg", msg);
 
 		//ディスパッチ処理
 		RequestDispatcher dispatch = request.getRequestDispatcher("/template/layout.jsp");
@@ -60,7 +106,6 @@ public class Attendance extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
